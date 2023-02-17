2a6db9f2fc9d75e6b2323e3dd9889fc99b63ce25
Anita Sri Handayani <anita.handa26@gmail.com>
2023-02-17 12:40:16 +0700

    add routes

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import Admin from '../components/adminComp.vue'
+
+const routes = [
+    // {
+    //     path: '/login',
+    //     name: 'Login',
+    //     component: Login,
+    // },
+    // {
+    //     path: '/',
+    //     name: 'Admin',
+    //     component: Admin,
+    // },
+
+]
+
+const router = createRouter({ history: createWebHistory(), routes })
+export default router
